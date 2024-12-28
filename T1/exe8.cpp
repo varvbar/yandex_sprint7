6251bb6e706d6a46b60234649ddd848e29d7d08e
Evgeny Panysev <varvbar88@gmail.com>
2024-12-28 16:24:01 +0500

    55

--- T1/exe8.cpp
+++ T1/exe8.cpp
@@ -1,24 +1,128 @@
 #include <string>
 #include <iostream>
+#include <list>
+#include <algorithm>
 
 
 class Editor {
 public:
-    Editor();
+    Editor(){
+        text_.push_back(cursor_);
+        cursor_pos_ = text_.begin();
+    };
+
     // сдвинуть курсор влево
-    void Left();
+    void Left(){
+        
+        auto temp_it = cursor_pos_;
+        auto temp_pos = cursor_pos_;
+
+        if (cursor_pos_ == text_.begin()){
+            return;
+        }
+        else {
+            text_.insert(--temp_it, '|');
+            text_.erase(temp_pos);
+            cursor_pos_ = temp_it;
+        }
+        return;
+    };
+
     // сдвинуть курсор вправо 
-    void Right();
+    void Right(){
+        
+        auto temp_it = cursor_pos_;
+        auto temp_pos = cursor_pos_;
+
+        if (cursor_pos_ == text_.end()){
+            return;
+        }
+        else {
+            std::advance(temp_it, 2);
+            text_.insert(temp_it, '|');
+            text_.erase(temp_pos);
+            cursor_pos_ = temp_it;
+        }
+        return;
+    }
+
     // вставить символ token
-    void Insert(char token);
+    void Insert(char token){
+        cursor_pos_ = text_.insert(cursor_pos_, token);
+        ++cursor_pos_;
+        return;
+    }
+
     // вырезать не более tokens символов, начиная с текущей позиции курсора
-    void Cut(std::size_t tokens = 1);
+    void Cut(std::size_t tokens = 1){
+       
+        buffer_.clear();
+        Copy(tokens);
+        
+        auto temp_it = cursor_pos_;
+        auto temp_pos = cursor_pos_;
+
+        std::advance(temp_it, tokens +1);
+        auto end_it = temp_it;
+        text_.erase(temp_pos,end_it);
+        cursor_pos_ = text_.insert(end_it, cursor_);
+        ++cursor_pos_;
+
+    }
+
     // cкопировать не более tokens символов, начиная с текущей позиции курсора
-    void Copy(std::size_t tokens = 1);
+    void Copy(std::size_t tokens = 1){
+        
+        buffer_.clear();
+        
+        auto temp_it = cursor_pos_;
+        std::advance(temp_it, tokens + 1);
+        auto end_it = temp_it;
+        temp_it = cursor_pos_;
+        
+        for(++temp_it; temp_it != end_it; ++temp_it){
+            if (temp_it == text_.end()){
+                buffer_.push_back(*temp_it);
+                break;
+            }
+            else {
+                buffer_.push_back(*temp_it);
+            }
+        }
+        return;
+    }
+
     // вставить содержимое буфера в текущую позицию курсора
-    void Paste();
+    void Paste(){
+        if (!buffer_.empty()) {
+            std::list temp_buffer = buffer_;
+            text_.splice(cursor_pos_, temp_buffer);
+            std::advance(cursor_pos_, buffer_.size());
+        }
+        else {
+            return;
+        }
+    }
+    
     // получить текущее содержимое текстового редактора
-    std::string GetText() const;
+    std::string GetText() const{
+        
+        std::string temp;
+        temp.reserve(text_.size() + 1);
+        
+        for(char ch : text_){
+            temp.push_back(ch);
+        }
+        return temp;
+    }
+
+private:
+    std::list<char> text_;
+    std::list<char> buffer_;
+    const char cursor_ = '|';
+
+    std::list<char>::iterator cursor_pos_;
+
 };
 
 int main() {
@@ -33,6 +137,7 @@ int main() {
     for (size_t i = 0; i < text.size(); ++i) {
         editor.Left();
     }
+
     // Текущее состояние редактора: `|hello, world`
     editor.Cut(7);
     // Текущее состояние редактора: `|world`
@@ -49,7 +154,8 @@ int main() {
     editor.Left();
     editor.Left();
     //Текущее состояние редактора: `world, hello|, `
-    editor.Cut(3);  // Будут вырезаны 2 символа
+    editor.Cut(2);  // Будут вырезаны 2 символа 
+
     // Текущее состояние редактора: `world, hello|`
     cout << editor.GetText();
     return 0;
